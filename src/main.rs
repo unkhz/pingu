c0dd6c6a4eeeb263570da00d08d9eb227d80191d
Juhani Pelli <juhani.pelli@gmail.com>
2022-02-27 11:31:58 +0200

    Extract responder module

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::Server;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, Worldo!".into()))
-}
+mod responder;
 
 #[tokio::main]
 async fn main() {
@@ -16,7 +14,7 @@ async fn main() {
     // creates one from our `hello_world` function.
     let make_svc = make_service_fn(|_conn| async {
         // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
+        Ok::<_, Infallible>(service_fn(responder::hello_world))
     });
 
     let server = Server::bind(&addr).serve(make_svc);
